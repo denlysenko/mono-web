38b8da9d18eb04e8f204657f49739cab4b9b9ccb
Denis <den_2710@ukr.net>
2015-11-30 23:12:51 +0200

    added about section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+(function($) {
+	'use strict';
+
+	var nav = {
+		selector: $('.navigation'),
+		init: function() {
+			var self = this;
+			self.selector.on('click', 'a', function(e) {
+				e.preventDefault();
+				self.selector.addClass('top');
+				$('.logo').addClass('hidden');
+				var href = $(this).attr('href');
+				$(this).addClass('active');
+				$(document).find(href).addClass('selected');
+			});
+		}
+	};
+
+	nav.init();
+
+})(jQuery);
