f871d7f445279a5a8f2acfa1c0867efc56d7c97a
Denis Lysenko <denis.lyisenko@qoderoom.com>
2015-12-10 17:58:40 +0200

    fixed gallery

--- app/scripts/gallery.js
+++ app/scripts/gallery.js
@@ -3,7 +3,7 @@
 
 	var Gallery = function(elem, itemNum) {
 		this.$elem = $(elem);
-		this.$items = this.$elem.find('.list > li');
+		this.$items = this.$elem.find('ul:first > li');
 		this.$list = this.$elem.find('.list');
 		this.height = this.$elem.height(); 
 		this.position = 0;
@@ -12,14 +12,15 @@
 
 	Gallery.prototype.moveDown = function(e, step) {
 		e.preventDefault();
-		this.position = Math.min((this.position - this.height)*step, -this.height);
-		this.$list.css('marginTop', this.position);
+		// this.position = Math.min((this.position - this.height)*step, -this.height);
+		// this.$list.css('marginTop', this.position);
 	};
 
-	Gallery.prototype.moveUp = function(e, step) {
+	Gallery.prototype.slide = function(e, step) {
 		e.preventDefault();
-		this.position = Math.min((this.position + this.height)*step, 0);
-		this.$list.css('marginTop', this.position);
+		// this.position = Math.min((this.position + this.height)*step, 0);
+		// this.$list.css('marginTop', this.position);
+		this.$elem.find('.list').removeClass('current').eq(step).addClass('current');
 	};
 
 	Gallery.prototype.generateIndicators = function() {
@@ -32,6 +33,19 @@
 		this.$elem.append(this.$indicators);
 	};
 
+	Gallery.prototype.split = function() {
+		var len = Math.ceil(this.$items.length/this.itemNum);
+		var wrapper = $('<div class="wrapper"></div>');
+		for(var i = 0; i < len; i++) {
+			var $list = $('<ul class="list"></ul>');
+			this.$items.slice(i*this.itemNum, i*this.itemNum + this.itemNum).detach().appendTo($list);
+			$list.appendTo(wrapper);
+		}
+		wrapper.appendTo(this.$elem);
+		this.$elem.find('ul:first').remove();
+		this.$elem.find('.list:first').addClass('current');
+	};
+
 	Gallery.prototype.addListeners = function() {
 		var self = this;
 		this.$indicators.on('click', 'a', function(e) {
@@ -41,7 +55,8 @@
 			var currentIndex = self.$indicators.find('li').index($current);
 			var targetIndex = self.$indicators.find('li').index($target);
 			if(currentIndex === targetIndex) return;
-			currentIndex > targetIndex ? self.moveUp(e, targetIndex) : self.moveDown(e, (targetIndex - currentIndex));
+			//currentIndex > targetIndex ? self.moveUp(e, targetIndex) : self.moveDown(e, (targetIndex - currentIndex));
+			self.slide(e, targetIndex);
 			$current.removeClass('current');
 			$target.addClass('current');
 		});
@@ -57,6 +72,7 @@
 
 	Gallery.prototype.init = function() {
 		this.generateIndicators();
+		this.split();
 		this.addListeners();
 	};
 
